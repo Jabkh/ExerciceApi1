b7d272ad97be2fe865bd073da2d843038fba7589
Jabkh <j.khaloua@hotmail.com>
2024-06-19 10:06:10 +0200

    Getone et GetAll fait

---
+++ src/main/java/org/example/exerciceapi/model/Voiture.java
@@ -0,0 +1,50 @@
+package org.example.exerciceapi.model;
+
+
+public class Voiture {
+    private int id;
+    private String marque;
+    private int anneeDeFabrication;
+    private String couleur;
+
+    public Voiture() {}
+
+    public Voiture(int id, String marque, int anneeDeFabrication, String couleur) {
+        this.id = id;
+        this.marque = marque;
+        this.anneeDeFabrication = anneeDeFabrication;
+        this.couleur = couleur;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public int getAnneeDeFabrication() {
+        return anneeDeFabrication;
+    }
+
+    public void setAnneeDeFabrication(int anneeDeFabrication) {
+        this.anneeDeFabrication = anneeDeFabrication;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+}
